5c1302863018abf7b672301bf3ace2ebba93097f
Cihan Erenler <cihanerenler@outlook.com>
2022-12-02 12:31:01 +0200

    implement initial auth to server

--- server/src/models/User.mjs
+++ server/src/models/User.mjs
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
-import Data from "./Data.mjs";
+import { Context } from "./Data.mjs";
 const { Schema } = mongoose;
 
 const User = new Schema({
-  name: { type: String, required: true },
-  lastname: { type: String, required: true },
-  password: { type: String, required: true },
-  email: { type: String, required: true },
-  data: { type: Data, required: false },
+	name: { type: String, required: true },
+	lastname: { type: String, required: true },
+	password: { type: String, required: true },
+	email: { type: String, required: true },
+	data: { type: Context, required: false },
 });
 
-export default mongoose.Modal("User", User);
+const UserModel = mongoose.model("UserModel", User);
+export default UserModel;
