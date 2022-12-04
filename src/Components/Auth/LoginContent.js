30c11f703061bc930a852c095e319e271d094a10
cihanerenler <cihanerenler@outlook.com>
2022-12-04 14:55:50 +0200

    fix styles on frontend and header

--- src/Components/Auth/LoginContent.js
+++ src/Components/Auth/LoginContent.js
@@ -7,64 +7,64 @@ import { useAuthContext } from "../../Context/AuthContext";
 import { toast } from "react-toastify";
 
 const LoginContent = () => {
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-	const { loginUser } = useAuthContext();
+  const { loginUser } = useAuthContext();
 
-	const handleClick = async (e) => {
-		e.preventDefault();
-		if (!email || !password) {
-			toast.warn("Please fill all fields");
-			return;
-		}
+  const handleClick = async (e) => {
+    e.preventDefault();
+    if (!email || !password) {
+      toast.warn("Please fill all fields");
+      return;
+    }
 
-		const credentials = { email, password };
-		await loginUser(credentials);
-	};
+    const credentials = { email, password };
+    await loginUser(credentials);
+  };
 
-	return (
-		<StyledLoginContent>
-			<h1>Login</h1>
-			<form>
-				<Input
-					placeholder="Email"
-					value={email}
-					action={(e) => setEmail(e.target.value)}
-					type="text"
-				/>
-				<Input
-					placeholder="Password"
-					value={password}
-					action={(e) => setPassword(e.target.value)}
-					type="password"
-				/>
-				<Button variant="full" action={handleClick}>
-					Login
-				</Button>
-			</form>
-			<p>
-				Don't you have an account? <Link to="/register">Register</Link>
-			</p>
-		</StyledLoginContent>
-	);
+  return (
+    <StyledLoginContent>
+      <h1>Login</h1>
+      <form>
+        <Input
+          placeholder="Email"
+          value={email}
+          action={(e) => setEmail(e.target.value)}
+          type="text"
+        />
+        <Input
+          placeholder="Password"
+          value={password}
+          action={(e) => setPassword(e.target.value)}
+          type="password"
+        />
+        <Button variant="full" action={handleClick}>
+          Login
+        </Button>
+      </form>
+      <p>
+        Don't you have an account? <Link to="/register">Join Notedock</Link>
+      </p>
+    </StyledLoginContent>
+  );
 };
 
 const StyledLoginContent = styled.div`
-	h1 {
-		font-size: 30px;
-		margin-bottom: 20px;
-		text-align: center;
-	}
+  h1 {
+    font-size: 30px;
+    margin-bottom: 20px;
+    text-align: center;
+  }
 
-	p {
-		margin-top: 10px;
-		font-size: 16px;
-		a {
-			text-decoration: none;
-			color: ${(props) => props.theme.primary};
-		}
-	}
+  p {
+    margin-top: 20px;
+    font-size: 14px;
+    a {
+      text-decoration: none;
+      color: ${(props) => props.theme.primary};
+    }
+  }
 `;
 
 export default LoginContent;
--- src/Components/Auth/RegisterContent.js
+++ src/Components/Auth/RegisterContent.js
@@ -8,83 +8,83 @@ import { useAuthContext } from "../../Context/AuthContext";
 import { useNavigate } from "react-router-dom";
 
 const RegisterContent = () => {
-	const [name, setName] = useState("");
-	const [lastname, setLastname] = useState("");
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
-	const navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-	const { createUser } = useAuthContext();
+  const { createUser } = useAuthContext();
 
-	const handleClick = async (e) => {
-		e.preventDefault();
-		if (!name || !lastname || !email || !password) {
-			toast.warn("Please fill all fields");
-			return;
-		}
+  const handleClick = async (e) => {
+    e.preventDefault();
+    if (!name || !lastname || !email || !password) {
+      toast.warn("Please fill all fields");
+      return;
+    }
 
-		const user = { name, lastname, email, password };
-		const response = await createUser(user);
-		console.log(response);
-		if (response.status === 200) {
-			navigate("/auth");
-		}
-	};
+    const user = { name, lastname, email, password };
+    const response = await createUser(user);
+    console.log(response);
+    if (response.status === 200) {
+      navigate("/auth");
+    }
+  };
 
-	return (
-		<StyledRegisterContent>
-			<form>
-				<h1>Register</h1>
-				<Input
-					placeholder="Name"
-					value={name}
-					action={(e) => setName(e.target.value)}
-					type="text"
-				/>
-				<Input
-					placeholder="Lastname"
-					value={lastname}
-					action={(e) => setLastname(e.target.value)}
-					type="text"
-				/>
-				<Input
-					placeholder="Email"
-					value={email}
-					action={(e) => setEmail(e.target.value)}
-					type="text"
-				/>
-				<Input
-					placeholder="Password"
-					value={password}
-					action={(e) => setPassword(e.target.value)}
-					type="password"
-				/>
-				<Button variant="full" action={handleClick}>
-					Register
-				</Button>
-			</form>
-			<p>
-				Already have an account? <Link to="/login">Login</Link>
-			</p>
-		</StyledRegisterContent>
-	);
+  return (
+    <StyledRegisterContent>
+      <form>
+        <h1>Register</h1>
+        <Input
+          placeholder="Name"
+          value={name}
+          action={(e) => setName(e.target.value)}
+          type="text"
+        />
+        <Input
+          placeholder="Lastname"
+          value={lastname}
+          action={(e) => setLastname(e.target.value)}
+          type="text"
+        />
+        <Input
+          placeholder="Email"
+          value={email}
+          action={(e) => setEmail(e.target.value)}
+          type="text"
+        />
+        <Input
+          placeholder="Password"
+          value={password}
+          action={(e) => setPassword(e.target.value)}
+          type="password"
+        />
+        <Button variant="full" action={handleClick}>
+          Register
+        </Button>
+      </form>
+      <p>
+        Already have an account? <Link to="/login">Login</Link>
+      </p>
+    </StyledRegisterContent>
+  );
 };
 
 const StyledRegisterContent = styled.div`
-	h1 {
-		font-size: 30px;
-		margin-bottom: 20px;
-		text-align: center;
-	}
+  h1 {
+    font-size: 30px;
+    margin-bottom: 20px;
+    text-align: center;
+  }
 
-	p {
-		margin-top: 10px;
-		font-size: 16px;
-		a {
-			text-decoration: none;
-			color: ${(props) => props.theme.primary};
-		}
-	}
+  p {
+    margin-top: 20px;
+    font-size: 14px;
+    a {
+      text-decoration: none;
+      color: ${(props) => props.theme.primary};
+    }
+  }
 `;
 
 export default RegisterContent;
