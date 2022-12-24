4a5690d166d81122fd69d2fd5675a137f0c9775d
cihanerenler <cihanerenler@outlook.com>
2022-12-24 17:16:41 +0200

    fix reset state bug

--- src/App.js
+++ src/App.js
@@ -12,57 +12,65 @@ import "react-toastify/dist/ReactToastify.css";
 import "./Split.css";
 
 function App() {
-	const [currentTheme, setCurrentTheme] = useState(false);
-	const { files, selectParent, parent, updateSelectedFile } =
-		useEditorContext();
-	const { getUserFromLocalStorage } = useAuthContext();
+  const [currentTheme, setCurrentTheme] = useState(false);
+  const { files, selectParent, parent, updateSelectedFile } =
+    useEditorContext();
+  const { getUserFromLocalStorage } = useAuthContext();
 
-	useEffect(() => {
-		getUserFromLocalStorage();
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
+  useEffect(() => {
+    getUserFromLocalStorage();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-	useEffect(() => {
-		if (files && files.items.length > 0) {
-			selectParent(files.items[0].id);
-		}
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [files]);
+  useEffect(() => {
+    if (files && files.items.length > 0) {
+      files.items.forEach((item) => {
+        if (item.items.length > 0) {
+          selectParent(item.id);
+          updateSelectedFile(item.items[0].id);
+        }
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [files]);
 
-	useEffect(() => {
-		if (parent && files) {
-			if (files.items[0].items.length > 0)
-				updateSelectedFile(files.items[0].items[0].id);
-		}
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [files]);
+  // useEffect(() => {
+  //   if (parent && files) {
+  // 		files.items.forEach(item => {
+  // 			if (item.length > 0)
+  //         updateSelectedFile(item[0].id);
+  // 		})
 
-	return (
-		<Main>
-			<GlobalStyles />
-			<ThemeProvider theme={currentTheme ? themes.dark : themes.default}>
-				<AuthWrapper>
-					<CustomRouter />
-				</AuthWrapper>
-			</ThemeProvider>
-			<ToastContainer
-				position="bottom-right"
-				autoClose={5000}
-				hideProgressBar={false}
-				newestOnTop={true}
-				transition={Slide}
-				rtl={false}
-				closeOnClick
-				pauseOnFocusLoss
-				draggable
-				pauseOnHover
-			/>
-		</Main>
-	);
+  //   }
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [files]);
+
+  return (
+    <Main>
+      <GlobalStyles />
+      <ThemeProvider theme={currentTheme ? themes.dark : themes.default}>
+        <AuthWrapper>
+          <CustomRouter />
+        </AuthWrapper>
+      </ThemeProvider>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={true}
+        transition={Slide}
+        rtl={false}
+        closeOnClick
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </Main>
+  );
 }
 
 const Main = styled.main`
-	height: 100vh;
+  height: 100vh;
 `;
 
 export default App;
--- src/Pages/RootLayout.js
+++ src/Pages/RootLayout.js
@@ -9,60 +9,63 @@ import AuthSpinner from "../Components/Auth/AuthSpinner";
 import { toast } from "react-toastify";
 
 const RootLayout = () => {
-	const location = useLocation();
-	const navigate = useNavigate();
-	const { user, showRootSpinner, handleRootSpinner, data } = useAuthContext();
-	const { setData, fetchData } = useEditorContext();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { user, showRootSpinner, handleRootSpinner } = useAuthContext();
+  const { setData, fetchData, clearState } = useEditorContext();
 
-	useEffect(() => {
-		if (user) {
-			toast.success(`Welcome ${user.name}`);
-			setTimeout(() => {
-				navigate("/notes");
-				handleRootSpinner();
-			}, 1500);
-		} else {
-			setTimeout(() => {
-				handleRootSpinner();
-			}, 1600);
-		}
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [user]);
+  useEffect(() => {
+    if (user) {
+      toast.success(`Welcome ${user.name}`);
+      setTimeout(() => {
+        navigate("/notes");
+        handleRootSpinner();
+      }, 1500);
+    } else {
+      setTimeout(() => {
+        handleRootSpinner();
+      }, 1600);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user]);
 
-	useEffect(() => {
-		if (!user) {
-			navigate("/");
-		}
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [user]);
+  useEffect(() => {
+    if (!user) {
+      clearState();
+      navigate("/");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user]);
 
-	useEffect(() => {
-		if (user && !data) {
-			fetchData(user);
-		} else if (data) {
-			setData(data);
-		}
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [user, data]);
+  useEffect(() => {
+    if (user) {
+      fetchData(user);
+    }
+    // else if (data) {
+    //   console.log("Set dataaaa");
+    //   setData(data);
+    // }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user]);
 
-	if (showRootSpinner) {
-		return <AuthSpinner />;
-	}
+  if (showRootSpinner) {
+    return <AuthSpinner />;
+  }
 
-	return (
-		<StyledRootElement>
-			{location.pathname !== "/notes" ? <Header /> : ""}
-			<main>
-				<Outlet />
-			</main>
-		</StyledRootElement>
-	);
+  return (
+    <StyledRootElement>
+      {location.pathname !== "/notes" ? <Header /> : ""}
+      <main>
+        <Outlet />
+      </main>
+    </StyledRootElement>
+  );
 };
 
 const StyledRootElement = styled.section`
-	main {
-		height: calc(100vh - 60px);
-	}
+  main {
+    height: calc(100vh - 60px);
+  }
 `;
 
 export default RootLayout;
